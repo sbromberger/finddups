261ce6ea63778c4ec6900562bb95fc06e5758eae
Seth Bromberger <github@bromberger.com>
2018-07-04 17:13:54 -0700

    comments and statistics

--- finddups.go
+++ finddups.go
@@ -6,18 +6,22 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 )
 
+// md5sum returns the md5sum of a file.
 func md5sum(file string) ([16]byte, error) {
-	dat, err := ioutil.ReadFile(file)
+	contents, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Println("error calculating md5sum for ", file)
 		return [16]byte{}, err
 	}
-	return md5.Sum(dat), nil
+	return md5.Sum(contents), nil
 }
 
-func processFile(path string, info os.FileInfo, err error, sizePath map[int64][]string) error {
+// processFile takes a path and updates sizePath, which is a map of
+// file size to the path.
+func processFile(path string, info os.FileInfo, err error, sizePath map[int64][]string, totalFiles *int) error {
 	if err != nil {
 		fmt.Println("error traversing ", path)
 		return err
@@ -27,20 +31,30 @@ func processFile(path string, info os.FileInfo, err error, sizePath map[int64][]
 		fmt.Println("error: cannot stat ", path)
 		return err
 	}
+	// don't try to get the size of directories
 	if st.IsDir() {
 		return nil
 	}
 	size := st.Size()
 	sizePath[size] = append(sizePath[size], path)
+	*totalFiles++
 	return nil
 }
 
-func processSizePath(sizePath map[int64][]string) [][]string {
+// processSizePath takes the sizePath map and
+// returns a list of lists, where the inner list is
+// a list of duplicate files.
+func processSizePath(sizePath map[int64][]string) ([][]string, int) {
+	totalmd5s := 0
 	dups := make([][]string, 0, 10)
 	for _, paths := range sizePath {
+		if len(paths) < 2 {
+			continue
+		}
 		pathmd5 := map[[16]byte][]string{}
 		for _, p := range paths {
 			md5p, err := md5sum(p)
+			totalmd5s++
 			if err == nil {
 				pathmd5[md5p] = append(pathmd5[md5p], p)
 			}
@@ -49,27 +63,33 @@ func processSizePath(sizePath map[int64][]string) [][]string {
 			dups = append(dups, dupPaths)
 		}
 	}
+	// cull all lists with only one file. There are no duplicates.
 	prunedDups := make([][]string, 0, 10)
 	for _, dup := range dups {
 		if len(dup) > 1 {
 			prunedDups = append(prunedDups, dup)
 		}
 	}
-	return prunedDups
+	return prunedDups, totalmd5s
 }
 
 func main() {
+	start := time.Now()
 	sizePath := make(map[int64][]string)
+	totalFiles := 0
+
 	fmt.Println("Starting")
 	root := os.Args[1]
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		return processFile(path, info, err, sizePath)
+		return processFile(path, info, err, sizePath, &totalFiles)
 	})
 	if err != nil {
 		fmt.Println("walkpath error")
 	}
-	dups := processSizePath(sizePath)
+	dups, totalmd5s := processSizePath(sizePath)
 	for _, dup := range dups {
 		fmt.Println("duplicate files: ", dup)
 	}
+	elapsed := time.Since(start)
+	fmt.Printf("total files processed: %d. Total md5s: %d. Took %s.\n", totalFiles, totalmd5s, elapsed)
 }
