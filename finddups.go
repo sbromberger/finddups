b3fc4b0ec73b7934d530cb1d3425a9e64fcd1d08
Seth Bromberger <github@bromberger.com>
2018-07-21 22:28:17 -0700

    working

--- finddups.go
+++ finddups.go
@@ -3,32 +3,31 @@ package main
 import (
 	"bufio"
 	"crypto/md5"
+	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
 	"time"
 
+	humanize "github.com/dustin/go-humanize"
 	log "github.com/sirupsen/logrus"
 )
 
-type md5Filename struct {
-	md5      [16]byte
-	filename string
+type hashFilename struct {
+	hash     [16]byte // md5 hash
+	filename string   // filename
 }
 
-// md5sum returns the md5sum of a file.
+type config struct {
+	minFileSize int64 // the minimum file size we care about
+	nWorkers    int   // number of workers to spawn
+}
+
+// md5sum returns the md5 hash of a file.
 func md5sum(file string) ([16]byte, error) {
-	contents, err := ioutil.ReadFile(file)
-	if err != nil {
-		log.Warn("error calculating md5sum for ", file)
-		return [16]byte{}, err
-	}
-	return md5.Sum(contents), nil
-}
-
-func md5sum2(file string) ([16]byte, error) {
 	h := md5.New()
 	f, err := os.Open(file)
 	if err != nil {
@@ -42,121 +41,112 @@ func md5sum2(file string) ([16]byte, error) {
 	return y, nil
 }
 
-// processFile takes a path and updates sizePath, which is a map of
-// file size to the path.
-func processFile(path string, info os.FileInfo, sizePath map[int64][]string, totalFiles *int) error {
-	// st, err := os.Stat(path)
-	// if err != nil {
-	// 	log.Warn("skipping invalid file ", path)
-	// 	return err
-	// }
-	// don't try to get the size of directories
-	if !info.Mode().IsRegular() {
-		return nil
-	}
-
-	size := info.Size()
-	sizePath[size] = append(sizePath[size], path)
-	*totalFiles++
-	return nil
-}
-
-func spawnWorker(filechan <-chan string, md5chan chan<- *md5Filename, md5fn func(string) ([16]byte, error)) {
-	for filename := range filechan {
-		filemd5, err := md5fn(filename)
+// launchWorker creates a worker that takes a file from a channel of
+// filenames, calculates the md5 of the file, and puts an md5Filename
+// struct to an output channel.
+func launchWorker(in <-chan string, out chan<- hashFilename, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for file := range in {
+		md5, err := md5sum(file)
 		if err == nil {
-			md5chan <- &md5Filename{filemd5, filename}
-		} else {
-			md5chan <- nil
+			hashinfo := hashFilename{hash: md5, filename: file}
+			out <- hashinfo
 		}
 	}
 }
 
-func enqueuePaths(filechan chan string, path string) {
-	filechan <- path
+// traverseDirectory walks a directory tree from a root and
+// checks file sizes. If two or more files have the same size,
+// the file names are pushed onto a channel for md5 hashing.
+func traverseDirectory(root string, fileChan chan<- string, cfg config) {
+
+	defer close(fileChan)
+	sizePath := make(map[int64][]string)
+
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.Mode().IsRegular() {
+			s := info.Size()
+			if s > cfg.minFileSize {
+				e := append(sizePath[s], path)
+				sizePath[s] = e
+				if len(e) > 1 { // this is the first or subsequent size match, send the path
+					fileChan <- path
+				}
+				if len(e) == 2 { // this is our first duplicate; send out the first file
+					fileChan <- e[0]
+				}
+			}
+		}
+		return nil
+	})
 }
 
-// processSizePath takes the sizePath map and
-// returns a list of lists, where the inner list is
-// a list of duplicate files.
-func processSizePath(sizePath map[int64][]string, md5fn func(string) ([16]byte, error)) ([][]string, int) {
-
-	totalmd5s := 0
-	for _, paths := range sizePath {
-		totalmd5s += len(paths)
-	}
-
-	md5chan := make(chan *md5Filename, 100) // can be nil
-	filechan := make(chan string, totalmd5s)
-	nWorkers := 10
-
-	for i := 0; i < nWorkers; i++ {
-		go spawnWorker(filechan, md5chan, md5sum2)
-	}
-	dups := make([][]string, 0, 10)
-	pathmd5 := map[[16]byte][]string{}
-
-	for _, paths := range sizePath {
-		if len(paths) < 2 {
-			continue
-		}
-
-		// at this point we have 2 or more files with the same size.
-		// put 'em in the channel.
-
-		for _, p := range paths {
-			go enqueuePaths(filechan, p)
-		}
-
-	}
-	fmt.Println("md5 total = ", totalmd5s)
-	for i := 0; i < totalmd5s; i++ {
-		md5ForFile := <-md5chan
-		if md5ForFile != nil {
-			pathmd5[md5ForFile.md5] = append(pathmd5[md5ForFile.md5], md5ForFile.filename)
-		}
-	}
-	fmt.Println("after dequeue")
-
-	for _, dupPaths := range pathmd5 {
-		dups = append(dups, dupPaths)
-	}
-
-	// cull all lists with only one file. There are no duplicates.
-	prunedDups := make([][]string, 0, 10)
-	for _, dup := range dups {
-		if len(dup) > 1 {
-			prunedDups = append(prunedDups, dup)
-		}
-	}
-	return prunedDups, totalmd5s
+// closer waits for all workers to exit and then closes the md5Filename channel.
+func closer(ch chan hashFilename, wg *sync.WaitGroup) {
+	defer close(ch)
+	wg.Wait()
 }
 
 func main() {
 	formatter := &log.TextFormatter{
 		FullTimestamp: true,
 	}
+
+	nCPU := runtime.NumCPU()
+	nWorkers := flag.Int("n", nCPU, fmt.Sprintf("number of workers (default %d)", nCPU))
+	minSizeStr := flag.String("min", "0", "minimum size of files to consider (default 0)")
+	flag.Parse()
+	minSize, err := humanize.ParseBytes(*minSizeStr)
+	if err != nil {
+		minSize = 0
+		log.Warn("Invalid minimum size specified; defaulting to 0")
+	}
+
+	cfg := config{nWorkers: *nWorkers, minFileSize: int64(minSize)}
 	log.SetFormatter(formatter)
 
 	log.Info("Starting")
 	start := time.Now()
-	sizePath := make(map[int64][]string)
-	totalFiles := 0
+	// make channels.
+	fileChan := make(chan string, cfg.nWorkers)
+	hashChan := make(chan hashFilename, cfg.nWorkers)
 
-	root := os.Args[1]
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		return processFile(path, info, sizePath, &totalFiles)
-	})
-	if err != nil {
-		log.Debug("walkpath error")
+	var wg sync.WaitGroup
+
+	wg.Add(cfg.nWorkers)
+
+	// launch workers to take file names and calculate md5s
+	for i := 0; i < cfg.nWorkers; i++ {
+		go launchWorker(fileChan, hashChan, &wg)
 	}
-	// dups, totalmd5s := processSizePath(sizePath)
-	dups, totalmd5s := processSizePath(sizePath, md5sum2)
-	for _, dup := range dups {
-		fmt.Println("duplicate files: ", dup)
+
+	go closer(hashChan, &wg)
+
+	root := flag.Args()[0]
+
+	go traverseDirectory(root, fileChan, cfg)
+
+	// we wait for md5 results to show up on the channel,
+	// and then add them to a map. The files that have the
+	// same md5s are presumed to be identical.
+	hashPath := make(map[[16]byte][]string)
+	for hashedfile := range hashChan {
+		hashPath[hashedfile.hash] = append(hashPath[hashedfile.hash], hashedfile.filename)
+	}
+
+	// go through the map, and for every md5 hash that has
+	// more than one file associated with it, print the files.
+	for _, dup := range hashPath {
+		if len(dup) > 1 {
+			fmt.Println("duplicate files: ", dup)
+		}
 	}
 	elapsed := time.Since(start)
-	log.Infof("Total files processed: %d.", totalFiles)
-	log.Infof("Total md5s: %d.", totalmd5s)
+	// log.Infof("Total files processed: %d.", totalFiles)
+	// log.Infof("Total md5s: %d.", totalmd5s)
 	log.Infof("Took %s.\n", elapsed.Round(1*time.Millisecond))
 }
